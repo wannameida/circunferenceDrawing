456eb422da2931579f63fa2224a7fd7b2c19b629
aluno <aluno@S2C59E5BEF396>
2023-10-26 10:46:22 -0300

    Adicionar arquivos de projeto.

---
+++ Home.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace circunferenceDraw
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(18, 19);
+            this.btnAbout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(182, 111);
+            this.btnAbout.TabIndex = 0;
+            this.btnAbout.Text = "Sobre";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 727);
+            this.Controls.Add(this.btnAbout);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbout;
+    }
+}
+
---
+++ Home.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace circunferenceDraw
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Não implementado", "Alerta do Sistema");
+            //System.Diagnostics.Process.Start("http://www.facebook.com");
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            DrawScreen drawScreen = new DrawScreen();
+
+        }
+    }
+}
